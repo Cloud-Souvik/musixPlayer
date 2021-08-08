42eb0d1a17dfcf2b16a776bd27d31c4573bd7a0d
Amirul Andalib <amirulandalib@gmail.com>
2021-08-08 19:06:06 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/moi-workspace/musixPlayer.git /MusicPlayer
+git clone https://amirulandalib:Amirul2021!@github.com/moi-workspace/musixPlayer.git /MusicPlayer
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
